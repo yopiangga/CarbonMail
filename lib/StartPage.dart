bcafd40c386bad18c1db2c192df9074c33492466
@yopigambyok <yopigambyok@gmail.com>
2022-01-13 23:11:10 +0700

    otw animation

--- lib/StartPage.dart
+++ lib/StartPage.dart
@@ -1,5 +1,6 @@
 import 'package:carbonmail/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({Key? key}) : super(key: key);
